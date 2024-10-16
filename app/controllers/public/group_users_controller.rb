5c9af0bca526c983084519400e19dda30a3a415f
jasmine2434 <echan2434@gmail.com>
2024-10-16 15:16:51 +0000

    group-user

--- app/controllers/public/group_users_controller.rb
+++ app/controllers/public/group_users_controller.rb
@@ -3,15 +3,25 @@ class Public::GroupUsersController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @group_user = current_user.group_users.new(group_id: params[:group_id])
-    @group_user.save
-    redirect_to request.referer
+    @group_user = GroupUser.new(group_id: params[:group_id], user_id: current_user.id)
+    if @group_user.save
+      flash[:notice] = "ユーザーをグループに追加しました"
+    else
+      flash[:alert] = "ユーザーを追加できませんでした"
+      redirect_to request.referer
+    end
   end
 
   def destroy
-    @group_user = current_user.group_users.find_by(group_id: params[:group_id])
-    @group_user.destroy
-    redirect_to request.referer
+    @group_user = GroupUser.find_by(group_id: params[:group_id], user_id: current_user.id)
+
+    if @group_user
+      @group_user.destroy
+      flash[:notice] = "グループから退会しました。"
+    else
+      flash[:alert] = "グループから退会できませんでした。"
+      redirect_to request.referer
+    end
   end
 
 end
