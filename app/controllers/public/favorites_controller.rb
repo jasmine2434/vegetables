964fef4c3e207c8db300227315a8f06ab595aada
jasmine2434 <echan2434@gmail.com>
2024-10-18 12:08:03 +0000

    favorite-show

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,22 @@
 class Public::FavoritesController < ApplicationController
 
 def create
-  vegetable = Vegetable.find(params[:vegetable_id])
-  favorite = current_user.favorites.new(vegetable_id: vegetable.id)
-  favorite.save
-  redirect_back(fallback_location: admin_admin_top_path)
+  @vegetable = Vegetable.find(params[:vegetable_id])
+  @favorite = current_user.favorites.new(vegetable_id: @vegetable.id)
+  @favorite.save
+  #redirect_back(fallback_location: admin_admin_top_path)
 end
 
 def destroy
-  vegetable = Vegetable.find(params[:vegetable_id])
-  favorite = current_user.favorites.find_by(vegetable_id: vegetable.id)
-  favorite.destroy
-  redirect_back(fallback_location: admin_admin_top_path)
+  @vegetable = Vegetable.find(params[:vegetable_id])
+  @favorite = current_user.favorites.find_by(vegetable_id: @vegetable.id)
+  @favorite.destroy
+  #redirect_back(fallback_location: admin_admin_top_path)
+end
+
+
+def vegetable_params
+  params.require(:vegetable).permit(:name, :body, :image, :genre_id)
 end
 
 end
